700214d75ea0d09a9c224a29efa84b413f09c90e
Saud BinGhushayan <Saudstg9@gmail.com>
2021-11-07 20:07:17 +0300

    push 2

--- TCPClient.java
+++ TCPClient.java
@@ -1,3 +1,21 @@
-public class TCPClient {
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
 
+public class TCPClient {
+    public static void main(String[] args) throws IOException {
+        Socket Client = new Socket("localhost", 0370);
+        System.out.println("Client Created");
+
+        BufferedReader ClientInput = new BufferedReader(new InputStreamReader(System.in));
+        System.out.println("Enter your phone number ");
+        String input = ClientInput.readLine();
+        PrintWriter ToServer = new PrintWriter(Client.getOutputStream(), true);
+        ToServer.println(input);
+        BufferedReader in = new BufferedReader(new InputStreamReader(Client.getInputStream()));
+        System.out.println(in.readLine());
+
+    }
 }
--- TCPServer.java
+++ TCPServer.java
@@ -1,3 +1,22 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 public class TCPServer {
 
+    public static void main(String[] args) throws IOException {
+        System.out.println("Establishing Clients ...");
+        ServerSocket Server = new ServerSocket(0370);
+        Socket ClientSocket = Server.accept();
+        System.out.println("Connection Created ");
+
+        BufferedReader inputfromClient = new BufferedReader(new InputStreamReader(ClientSocket.getInputStream()));
+        String str = inputfromClient.readLine();
+        PrintWriter outToClient = new PrintWriter(ClientSocket.getOutputStream(), true);
+        outToClient.println("Server says " + str);
+
+    }
 }
